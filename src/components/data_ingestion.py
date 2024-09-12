7c42b70715e76c51b57e740d81da436c74829487
tanyasree03 <tanyasree2003@gmail.com>
2024-09-13 00:24:16 +0530

    Pipelining

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,7 @@ import sys
 
 #sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from src.exception import CustomException
+from ..exception import CustomException
 
 from src.logger import logging
 import pandas as pd
